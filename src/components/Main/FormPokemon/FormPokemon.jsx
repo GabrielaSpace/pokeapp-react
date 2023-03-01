0915a2536f9e709abd09a3a71f5271434440ba27
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-03-01 17:03:23 +0100

    update f1&2

--- src/components/Main/FormPokemon/FormPokemon.jsx
+++ src/components/Main/FormPokemon/FormPokemon.jsx
@@ -1,68 +1,53 @@
-import { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CardPokemon from '../ListPokemon/CardPokemon/CardPokemon'
+import { v4 as uuidv4 } from 'uuid';
+import CardPokemon from '../ListPokemon/CardPokemon/CardPokemon';
 
 function FormPokemon(props) {
   const [input, setInput] = useState('');
   const [pokemonData, setPokemonData] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
-
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const getPokemonData = async () => {
       if (input.trim() === '') {
         setErrorMessage('Please, enter a name or id pokemon');
-        setPokemonData([]);
         return;
       }
       try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
-        setPokemonData(response.data);
+        setPokemonData([...pokemonData, response.data]);
       } catch (error) {
         setErrorMessage('Pokemon not found');
-        setPokemonData([]);
       }
     };
     getPokemonData();
   }, [input]);
 
-  const handleChange = (event) => {
+  const handleChangeInput = (event) => {
     setInput(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (Object.keys(pokemonData).length === 0) {
-      setErrorMessage('Please, enter a name or id pokemon');
-      return;
-    }
-    const newPokemon = {
-      id: uuidv4(),
-      name: pokemonData.name,
-      height: pokemonData.height,
-      weight: pokemonData.weight,
-      sprites: {
-        front_default: pokemonData.sprites.front_default,
-        back_default: pokemonData.sprites.back_default
-      },
-      base_experience: pokemonData.base_experience
-    }
+    setSearch(input);
     setInput('');
-    setPokemonData([...pokemonData, newPokemon]); 
   };
-  
+
+  const printList = () => {
+    return pokemonData.map((pokemon) => <CardPokemon pokemon={pokemon} key={uuidv4()} />);
+  };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
       <h1>Search your pokemon</h1>
-      <input placeholder="Name or Id of the pokemon" name="text" type="text" value={input} onChange={handleChange} />
+      <input placeholder="Name or Id of the pokemon" name="name" type="text" value={input} onChange={handleChangeInput} />
       <button type="submit">search</button>
       {errorMessage !== '' && <div className="alert">{errorMessage}</div>}
-      {Object.keys(pokemonData).length > 0 && <CardPokemon pokemon={pokemonData} />}
+      {printList()}
     </form>
   );
 }
 
 export default FormPokemon;
-
--- src/components/Main/ListPokemon/CardPokemon/CardPokemon.jsx
+++ src/components/Main/ListPokemon/CardPokemon/CardPokemon.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function CardPokemon(props) {
   const { pokemon } = props;
 
@@ -10,13 +9,11 @@ function CardPokemon(props) {
       <img src={pokemon.sprites.back_default} alt={pokemon.name} />
       <h3>{pokemon.name.toUpperCase()}</h3>
       <p>Id: {pokemon.id}</p>
-      <p>Experience: {pokemon.base_experience}</p> 
-      <p>Height: {pokemon.height/10} m</p> 
-      <p>Weight: {pokemon.weight/10} Kg</p>
-     
+      <p>Experience: {pokemon.base_experience}</p>
+      <p>Height: {pokemon.height / 10} m</p>
+      <p>Weight: {pokemon.weight / 10} Kg</p>
     </article>
   );
 }
 
 export default CardPokemon;
-

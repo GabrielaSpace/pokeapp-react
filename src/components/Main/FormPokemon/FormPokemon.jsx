ad8e50dd730e1c0406bb438c98a2dc0f669a34e6
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-03-01 20:18:04 +0100

    debounce done

--- src/components/Main/FormPokemon/FormPokemon.jsx
+++ src/components/Main/FormPokemon/FormPokemon.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import { useDebounce } from 'use-debounce'
 import CardPokemon from '../ListPokemon/CardPokemon/CardPokemon';
 
 function FormPokemon(props) {
@@ -8,22 +9,27 @@ function FormPokemon(props) {
   const [pokemonData, setPokemonData] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [search, setSearch] = useState('');
+  const [debouncedInput] = useDebounce(input, 2000)
 
   useEffect(() => {
     const getPokemonData = async () => {
-      if (input.trim() === '') {
+      if (debouncedInput.trim() === '') {
         setErrorMessage('Please, enter a name or id pokemon');
         return;
       }
+      if (pokemonData.some(pokemon => pokemon.name === debouncedInput.toLowerCase() || pokemon.id === parseInt(debouncedInput))) {
+        setErrorMessage('Pokemon already in the list');
+        return;
+      }
       try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
-        setPokemonData([...pokemonData, response.data]);
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${debouncedInput}`);
+        setPokemonData([response.data, ...pokemonData]);
       } catch (error) {
         setErrorMessage('Pokemon not found');
       }
     };
     getPokemonData();
-  }, [input]);
+  }, [debouncedInput]);
 
   const handleChangeInput = (event) => {
     setInput(event.target.value);
@@ -43,7 +49,6 @@ function FormPokemon(props) {
     <form className="form" onSubmit={handleSubmit}>
       <h1>Search your pokemon</h1>
       <input placeholder="Name or Id of the pokemon" name="name" type="text" value={input} onChange={handleChangeInput} />
-      <button type="submit">search</button>
       {errorMessage !== '' && <div className="alert">{errorMessage}</div>}
       {printList()}
     </form>
